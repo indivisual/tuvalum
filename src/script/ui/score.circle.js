76120cb888efdba4ccd96d69a537701ff5efdfd9
indivisual <carlos@indivisual.es>
2017-12-25 23:14:56 +0100

    avance en catalogo

---
+++ src/script/ui/score.circle.js
@@ -0,0 +1,33 @@
+var Tvm = Tvm || {};
+
+Tvm.ScoreCircle = (function(){
+
+    var _api = {};
+    var RADIUS = 41;
+    var CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+
+	function init() {
+        $('.score-circle').each(function() {
+            $(this).find('.progress__value').css('strokeDasharray', CIRCUMFERENCE);
+            _api.progress($(this));
+        });
+		
+		
+	}
+	
+    _api.progress = function(elem, value) {
+        if(value == undefined) var value = elem.attr('data-score');
+        
+        var progress = value / 100;
+        var dashoffset = CIRCUMFERENCE * (1 - progress);
+        
+        console.log('progress:', value + '%', '|', 'offset:', dashoffset)
+        
+        elem.find('.progress__value').css('strokeDashoffset', dashoffset);
+    }
+
+	if($('.score-circle').length) init();
+	return _api;
+
+})();
+
